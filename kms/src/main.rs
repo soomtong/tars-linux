6b5bd867fa8b08b4f60ef01ae4a934ecbb22f24d
YoungTaek <soomtong@gmail.com>
2026-08-07 22:49:04 +0900

    Create dumb buffer and fill it with solid red in kms

--- kms/src/main.rs
+++ kms/src/main.rs
@@ -79,6 +79,26 @@ struct DrmModeGetEncoder {
     possible_clones: u32,
 }
 
+#[repr(C)]
+#[derive(Debug, Default)]
+struct DrmModeCreateDumb {
+    height: u32,
+    width: u32,
+    bpp: u32,
+    flags: u32,
+    handle: u32,
+    pitch: u32,
+    size: u64,
+}
+
+#[repr(C)]
+#[derive(Debug, Default)]
+struct DrmModeMapDumb {
+    handle: u32,
+    pad: u32,
+    offset: u64,
+}
+
 unsafe fn drm_ioctl<T>(fd: i32, request: libc::c_ulong, arg: &mut T) -> io::Result<()> {
     let ret = libc::ioctl(fd, request, arg as *mut T as *mut libc::c_void);
     if ret < 0 {
@@ -191,7 +211,7 @@ fn main() -> io::Result<()> {
     let fd = file.as_raw_fd();
 
     let (crtc_ids, connector_ids, _encoder_ids) = get_resources(fd)?;
-    let (connector, _mode, encoders) = find_connected_connector(fd, &connector_ids)?;
+    let (connector, mode, encoders) = find_connected_connector(fd, &connector_ids)?;
     let encoder_id = if connector.encoder_id != 0 {
         connector.encoder_id
     } else {
@@ -202,6 +222,51 @@ fn main() -> io::Result<()> {
     let crtc_id = find_crtc(fd, encoder_id, &crtc_ids)?;
 
     println!("kms: selected crtc {}", crtc_id);
+
+    let mut dumb = DrmModeCreateDumb {
+        height: mode.vdisplay as u32,
+        width: mode.hdisplay as u32,
+        bpp: 32,
+        ..Default::default()
+    };
+    unsafe { drm_ioctl(fd, drm_iowr(0xB2, size_of::<DrmModeCreateDumb>()), &mut dumb)? };
+    println!(
+        "kms: dumb buffer handle={} pitch={} size={}",
+        dumb.handle, dumb.pitch, dumb.size
+    );
+
+    let mut map = DrmModeMapDumb {
+        handle: dumb.handle,
+        ..Default::default()
+    };
+    unsafe { drm_ioctl(fd, drm_iowr(0xB3, size_of::<DrmModeMapDumb>()), &mut map)? };
+
+    let map_ptr = unsafe {
+        libc::mmap(
+            ptr::null_mut(),
+            dumb.size as usize,
+            libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_SHARED,
+            fd,
+            map.offset as libc::off_t,
+        )
+    };
+    if map_ptr == libc::MAP_FAILED {
+        return Err(io::Error::last_os_error());
+    }
+
+    let red: u32 = 0x00FF_0000;
+    for row in 0..dumb.height as usize {
+        let row_start = row * dumb.pitch as usize;
+        for col in 0..dumb.width as usize {
+            let offset = row_start + col * 4;
+            unsafe {
+                let pixel_ptr = (map_ptr as *mut u8).add(offset) as *mut u32;
+                pixel_ptr.write_volatile(red);
+            }
+        }
+    }
+    println!("kms: filled framebuffer with solid red");
 
     Ok(())
 }
