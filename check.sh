90d90d163e3526e2bf697ebe641b18c3c7a1a173
YoungTaek <soomtong@gmail.com>
2026-08-26 13:47:58 +0900

    Rename the copy chain for CM-M2

--- check.sh
+++ check.sh
@@ -106,6 +106,6 @@ run_chain "IP-M2" ./input/check.sh
 run_chain "PM-M1" ./power/check.sh
 run_chain "HD-M2" ./device/check.sh
 run_chain "TR-M2" ./render/check.sh
-run_chain "CM-M1" ./copy/check.sh
+run_chain "CM-M2" ./copy/check.sh
 
 echo "TARS check PASS: all chains 3/3 consecutive runs succeeded"
